8656a52f9a333531b7bcabd543d7cd4ce0ca81ce
James Robinson <jrstats@outlook.com>
2023-10-06 17:30:41 +0100

    separate datetime widget

--- app/main.py
+++ app/main.py
@@ -2,6 +2,7 @@ from fastapi import FastAPI, Request
 from fastapi.templating import Jinja2Templates
 from datetime import datetime
 from resources.sqlite import get_all_sql_widgets_html
+from widgets.datetime.datetime import get_datetime_widget
 
 app = FastAPI()
 templates = Jinja2Templates(directory="app/templates")
@@ -11,14 +12,9 @@ async def home(request: Request):
     # Your logic to retrieve and process data goes here
     
     # Example time and date retrieval:
-    current_time = datetime.now().strftime("%H:%M")
-    current_date = datetime.now().strftime("%Y-%m-%d")
+    datetime_widget = get_datetime_widget()
     right_widgets = get_all_sql_widgets_html(["bins", "weather_current", "weather_daily"])
-    left_widgets = [
-        f"<div>Time: {current_time}</div>",
-        f"<div>Date: {current_date}</div>",
-        # Add more left widgets here
-    ] + get_all_sql_widgets_html(["tfl_status"])
+    left_widgets = [datetime_widget] + get_all_sql_widgets_html(["tfl_status"])
 
 
     return templates.TemplateResponse(
