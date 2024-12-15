6d63cbfc0c028bdcb1b580db1c71563448692bd9
James Robinson <james@ember-climate.org>
2024-12-15 12:40:48 +0000

    replace selenium resource

--- dags/resources/resources.py
+++ dags/resources/resources.py
@@ -2,14 +2,24 @@ from contextlib import contextmanager
 import dagster as dg
 from pydantic import PrivateAttr
 from selenium.webdriver.chrome.webdriver import WebDriver
-
-from resources.selenium import SeleniumDriver
+from selenium import webdriver
+from selenium.webdriver import ChromeOptions
+from selenium.webdriver.chrome.service import Service
+from webdriver_manager.chrome import ChromeDriverManager
 
 class SeleniumResource(dg.ConfigurableResource):
     _driver: WebDriver = PrivateAttr()
 
+    def _set_options(self):
+        self.options = ChromeOptions()
+
+    def _set_driver(self):
+        self._set_options()
+        driver_service = Service(ChromeDriverManager().install())
+        self._driver = webdriver.Chrome(service=driver_service, options=self.options)
+
     @contextmanager
     def yield_for_execution(self, context: dg.InitResourceContext):
-        with SeleniumDriver() as driver:
-            self._driver = driver
-            yield self
+        self._set_driver()
+        yield self
+        self._driver.close()
--- resources/sqlite.py
+++ resources/sqlite.py
@@ -1,85 +1,17 @@
-import duckdb
-import os
-import pandas as pd
-import datetime as dt
-from typing import Callable
-from contextlib import contextmanager
-from dotenv import dotenv_values
+import dagster as dg
+from importlib import import_module
 
 
-@contextmanager
-def get_db_con():
-    env = dotenv_values()
-    with duckdb.connect(database=env["SQLITE"]) as con:
-        yield con
-
-
-def get_sql_widget_html(widget_name):
-    with get_db_con() as con:
-        cursor = con.cursor()
-        cursor.execute(
-            "SELECT html_content FROM widgets WHERE widget_name=?", (widget_name,)
-        )
-        widget = cursor.fetchone()
+def get_sql_widget_html(asset_key: dg.AssetKey):
+    dags = import_module("dags")
+    defs : dg.Definitions = dags.defs
 
     try:
-        return widget[0]
-    except (IndexError, TypeError):
-        return f"<div>No data for {widget_name} widget</div>"
+        return defs.load_asset_value(asset_key)
+    except FileNotFoundError:
+        return f"<div>No data for {asset_key.path[-1]} widget</div>"
 
 
 def get_all_sql_widgets_html(widget_names):
     widgets = [get_sql_widget_html(w) for w in widget_names]
     return widgets
-
-
-def update_sql_html(widget_name, html) -> None:
-    updated_timestamp = dt.datetime.now()
-    sql_update = """
-    INSERT OR REPLACE INTO widgets (widget_name, html_content, updated)
-    VALUES (?, ?, ?);
-    """
-
-    with get_db_con() as con:
-        cursor = con.cursor()
-        cursor.execute(sql_update, (widget_name, html, updated_timestamp))
-        con.commit()
-
-
-def update_sql_df(widget_name: str, df: pd.DataFrame):
-    with get_db_con() as con:
-        df.to_sql(widget_name, con=con, if_exists="replace", index=False)
-
-
-def update_sql_widget(
-    widget_name: str, df: pd.DataFrame, fn: Callable, **kwargs
-) -> None:
-    update_sql_df(widget_name, df)
-    df_html = fn(df, **kwargs)
-    update_sql_html(widget_name, df_html)
-    return None
-
-
-def instantiate():
-    # Connect to SQLite database. If it doesn't exist, it will be created.
-    with get_db_con() as con:
-        # Create a new SQLite cursor.
-        cursor = con.cursor()
-
-        # Create `widgets` table.
-        cursor.execute(
-            """
-        CREATE TABLE IF NOT EXISTS widgets (
-            widget_name TEXT PRIMARY KEY,
-            html_content TEXT NOT NULL,
-            updated TIMESTAMP NOT NULL
-        )
-        """
-        )
-
-        # Commit the changes and close the connection.
-        con.commit()
-
-
-if __name__ == "__main__":
-    instantiate()
